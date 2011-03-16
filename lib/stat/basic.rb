f788d5e31c17afa539a87a058831e953eccf10ac
David Jacobs <david@allthingsprogress.com>
2011-03-15 23:58:39 -0400

    make all methods class methods

--- lib/stat/basic.rb
+++ lib/stat/basic.rb
@@ -1,15 +1,15 @@
 module Stat
   module Basic
-    def arithmetic_mean(values)
+    def self.arithmetic_mean(values)
       values.reduce(&:+) / values.length
     end
     alias_method :mean, :arithmetic_mean
 
-    def geometric_mean(values)
+    def self.geometric_mean(values)
       values.reduce(&:*) ** (1.0/values.length)
     end
     
-    def median(values)
+    def self.median(values)
       n = values.length
       if n.odd? 
         values.sort[n / 2]
@@ -19,7 +19,7 @@ module Stat
       end
     end
 
-    def mode(values)
+    def self.mode(values)
       # Maybe there's a better way to do this...
       values.sort.
         group_by {|i| i }.
@@ -27,28 +27,28 @@ module Stat
         first
     end
 
-    def variance(values, type=:population)
+    def self.variance(values, type=:population)
       n = type == :population ? values.length : values.length - 1
       mean = mean values
       1.0 / n * values.reduce {|x| (x - mean) ** 2 }
     end
 
-    def std(values, type=:population)
+    def self.std(values, type=:population)
       Math.sqrt variance(values, type)
     end
 
-    def stderr(values)
+    def self.stderr(values)
       n = values.length
       std(values, :sample) / sqrt(n)
     end
     alias :sem, :stderr
 
-    def relative_stderr(values)
+    def self.relative_stderr(values)
       stderr(values) / mean(values) * 100
     end
     alias :rse, :relative_stderr
 
-    def coefficient_of_variation(values, type=:population)
+    def self.coefficient_of_variation(values, type=:population)
       std(values, type) / mean(values) * 100
     end
     alias :cv, :coefficient_of_variation
