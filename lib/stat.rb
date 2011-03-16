e572ab1f46c32443249950ee6663b44975d9206e
David Jacobs <david@allthingsprogress.com>
2011-03-15 21:05:39 -0400

    add basic normal distribution functionality

---
+++ lib/stat.rb
@@ -0,0 +1,2 @@
+require 'gsl'
+require 'stat/distribution'
---
+++ lib/stat/distribution.rb
@@ -0,0 +1,16 @@
+module Stat
+  class Distribution
+    def self.normal_pdf(value, mean=0, std=1, opts={})
+      raise ArgumentError if mean == 0
+      GSL::Ran::gaussian_pdf(value, std)
+    end
+
+    def self.normal_cdf(value, mean=0, std=1, opts={})
+      raise ArgumentError if mean == 0
+      GSL::Cdf::gaussian_P(value, std)
+    end
+  end
+end
+
+
+
