51e1aa7ae058c16c100f57f48cb13c572d031924
David Jacobs <david@allthingsprogress.com>
2011-04-07 13:23:12 -0400

    make class method aliases work, fix alias -> alias_method

--- lib/stat/basic.rb
+++ lib/stat/basic.rb
@@ -1,56 +1,76 @@
 module Stat
   module Basic
-    def self.arithmetic_mean(values)
-      values.reduce(&:+) / values.length
-    end
-    alias_method :mean, :arithmetic_mean
-
-    def self.geometric_mean(values)
-      values.reduce(&:*) ** (1.0/values.length)
-    end
-    
-    def self.median(values)
-      n = values.length
-      if n.odd? 
-        values.sort[n / 2]
-      else
-        index = n / 2 - 1
-        mean values.sort[index, 2]
+    class << self
+      def arithmetic_mean(values)
+        return nil if values.empty?
+        values.reduce(&:+).to_f / values.length
       end
-    end
+      alias_method :mean, :arithmetic_mean
 
-    def self.mode(values)
-      # Maybe there's a better way to do this...
-      values.sort.
-        group_by {|i| i }.
-        max_by {|k,v| v.length }.
-        first
-    end
+      def geometric_mean(values)
+        return nil if values.empty?
+        values.reduce(&:*) ** (1.0 / values.length)
+      end
+      
+      def median(values)
+        return nil if values.empty?
+        n = values.length
+        if n.odd? 
+          values.sort[n / 2]
+        else
+          index = n / 2 - 1
+          mean values.sort[index, 2]
+        end
+      end
 
-    def self.variance(values, type=:population)
-      n = type == :population ? values.length : values.length - 1
-      mean = mean values
-      1.0 / n * values.reduce {|x| (x - mean) ** 2 }
-    end
+      def mode(values)
+        return nil if values.empty?
 
-    def self.std(values, type=:population)
-      Math.sqrt variance(values, type)
-    end
+        # Maybe there's a better way to do this...
+        
+        identity = lambda {|x| x }
+        groups = values.group_by(&identity).values
+        counts = groups.group_by(&:length)
+        max_count = counts.keys.sort.last
+        mode = counts[max_count].map {|x| x.first }
 
-    def self.stderr(values)
-      n = values.length
-      std(values, :sample) / sqrt(n)
-    end
-    alias :sem, :stderr
+        case mode.length
+        when values.length
+          nil
+        when 1
+          mode.first
+        else
+          mode
+        end
+      end
 
-    def self.relative_stderr(values)
-      stderr(values) / mean(values) * 100
-    end
-    alias :rse, :relative_stderr
+      def variance(values, type=:population)
+        return nil if values.empty?
 
-    def self.coefficient_of_variation(values, type=:population)
-      std(values, type) / mean(values) * 100
+        n = type == :population ? values.length : values.length - 1
+        mean = mean values
+        1.0 / n * values.reduce {|x| (x - mean) ** 2 }
+      end
+
+      def std(values, type=:population)
+        Math.sqrt variance(values, type)
+      end
+
+      def stderr(values)
+        n = values.length
+        std(values, :sample) / sqrt(n)
+      end
+      alias_method :sem, :stderr
+
+      def relative_stderr(values)
+        stderr(values) / mean(values) * 100
+      end
+      alias_method :rse, :relative_stderr
+
+      def coefficient_of_variation(values, type=:population)
+        std(values, type) / mean(values) * 100
+      end
+      alias_method :cv, :coefficient_of_variation
     end
-    alias :cv, :coefficient_of_variation
   end
 end
