7065579cd59b3fe2426a4f49ae5ffd4265da4ff8
David Jacobs <david@allthingsprogress.com>
2011-04-29 16:41:02 -0400

    remove comment

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,6 @@
 require 'rspec'
 require 'stats'
 
-# Passes for values approximately equal to each other,
-# using a tolerance of 0.00001.
 RSpec::Matchers.define :be_pseudo_equal do |num2|
   match do |num1|
     epsilon = 0.00001
