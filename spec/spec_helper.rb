12c84819d89bfbefff81433cc245e14f2695718e
David Jacobs <david@wit.io>
2011-08-23 00:00:40 -0700

    lower epsilon for now

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ require 'stats'
 
 RSpec::Matchers.define :be_pseudo_equal do |num2|
   match do |num1|
-    epsilon = 0.00001
+    epsilon = 0.0001
     (num1 - num2).abs.should < epsilon
   end
 end
