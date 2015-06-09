c621e53aacd502edd221aa5c4ffbfcf573aaf534
David Jacobs <david@wit.io>
2015-06-08 22:24:25 -0700

    Fix test deprecation.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,14 @@
 require 'rspec'
 require 'stats'
 
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    # c.syntax = :expect             # disables `should`
+    # c.syntax = [:should, :expect]  # default, enables both `should` and `expect`
+    c.syntax = :should             # disables `expect`
+  end
+end
+
 RSpec::Matchers.define :be_pseudo_equal do |num2|
   match do |num1|
     epsilon = 0.0001
